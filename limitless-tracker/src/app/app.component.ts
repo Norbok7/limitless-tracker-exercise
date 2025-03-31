674c4f78952a659301c35d99f66c0d490cbba232
Garrett Duncan <duncansports_17@hotmail.com>
2025-03-31 18:49:29 -0500

    add dashboard and exercise components with styling and functionality

--- limitless-tracker/src/app/app.component.ts
+++ limitless-tracker/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { ExerciseListComponent } from './features/exercises/components/exercise-list/exercise-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DashboardPageComponent, ExerciseListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
